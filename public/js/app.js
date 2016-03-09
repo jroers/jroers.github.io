ce3da487272e600654f643242d5aa6c81c47cb04
jroers <joe.roers@gmail.com>
2016-03-09 09:34:31 -0800

    Removed js sanity check, updated resume

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-	console.log("JS is working!");
 	$(function() {
 		$('a[href*="#"]:not([href="#"])').click(function() {
 	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
